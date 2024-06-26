f9bde1b022aa32554ff7f9480b91d9d783ae885a
joshua <joshua@naver.com>
2024-06-26 11:10:11 +0900

    ..

--- src/main/java/com/example/socar/admin/AdminService.java
+++ src/main/java/com/example/socar/admin/AdminService.java
@@ -12,8 +12,8 @@ import java.util.List;
 요약
 Controller (쿠키 가게): 웹 페이지를 만들어서 화면에 보여주는 역할.
 RestController (케이크 배달 가게): 필요한 데이터를 직접 전달해주는 역할. 화면은 보여주지 않아요.
- * @ResponseBody 없이: 요청을 받으면 웹 페이지를 만들어서 보여줌.
- * @ResponseBody 사용: 요청을 받으면 데이터를 그대로 돌려줌.
+ @ResponseBody 없이: 요청을 받으면 웹 페이지를 만들어서 보여줌.
+ @ResponseBody 사용: 요청을 받으면 데이터를 그대로 돌려줌.
  * */
 
 @Service
@@ -23,7 +23,7 @@ public class AdminService {
     private AdminRepository adminRepository;
 
 
-    public Admin getAdminByAdminId(String adminId){
+    public Admin getAdminByAdminId(String adminId) {
         return adminRepository.findByAdminId(adminId);
     }
 
--- src/main/java/com/example/socar/jwt/JwtFilter.java
+++ src/main/java/com/example/socar/jwt/JwtFilter.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -37,7 +38,7 @@ public class JwtFilter extends OncePerRequestFilter {
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             if (jwtUtil.validateToken(token, username)) {
-                var userDetails = adminDetailsService.loadUserByUsername(username);
+                UserDetails userDetails = adminDetailsService.loadUserByUsername(username);
                 var authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authToken);
